5ffb3f1976fce3b1c014c5c785a03ba9cb674b5e
“sunilsamal322” <sunilsamal322@gmail.com>
2026-03-26 00:33:34 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
 import logger from "./config/logger.js";
+import connectDB from "./config/mongodb.js";
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, async () => {
+  await connectDB();
   logger.info(`Server running on port ${PORT}`);
 });
 
